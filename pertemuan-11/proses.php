df6a3b36c82cd7cdb59b6a5f407841775efb1576
unknown <2511500030@mahasiswa.atmaluhur.ac.id>
2025-12-13 02:03:41 +0700

    rewrite proses.php

--- pertemuan-11/proses.php
+++ pertemuan-11/proses.php
@@ -9,8 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nama  = bersihkan($_POST['txtNama']) ?? '';
     $email = bersihkan($_POST['txtEmail']) ?? '';
     $pesan = bersihkan($_POST['txtPesan'] ?? '');
-
-   
+ 
 $errors = []; 
 if ($nama === '') {
     $errors[] = 'Nama wajib diisi.';
@@ -37,3 +36,29 @@ if (!empty($errors)) {
     $_SESSION['flash_error'] = implode('<br>', $errors);
     redirect_ke('index.php#contact');
 }
+
+$sql = "INSERT INTO tbl_tamu (cnama, cemail, cpesan) VALUES (?, ?, ?)";
+$stmt = mysqli_prepare($conn, $sql);
+
+if (!$stmt) {
+  $_SESSION['flash_error'] = 'Terjadi kesalahan sistem (prepare gagal).';
+  redirect_ke('index.php#contact');
+}
+   
+mysqli_stmt_bind_param($stmt, 'sss', $nama, $email, $pesan);
+
+if (mysqli_stmt_execute($stmt)) {
+  unset($_SESSION['old']);
+    $_SESSION['flash_sukses'] = 'Terima kasih, data anda sudah disimpan.';
+} else {
+  $_SESSION['old'] = [
+      'nama'  => $nama,
+      'email' => $email,
+      'pesan' => $pesan,
+  ];
+    $_SESSION['flash_error'] = 'Data gagal disimpan. Silahkan coba lagi.';
+    redirect_ke('index.php#contact');
+}
+
+mysqli_stmt_close($stmt);
+
